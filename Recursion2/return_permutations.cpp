16fc670f1e81b91cb36eb7aae6b402e4d95cbfbe
ShreyV25 <shrey.verma2509@gmail.com>
2021-03-17 02:35:03 +0530

    .

---
+++ Recursion2/return_permutations.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int returnPermutations(string i, string o[])
+{
+    
+}
+
+int main()
+{
+    string input;
+    cin >> input;
+    string output[10000];
+    int count = returnPermutations(input, output);
+    for(int i = 0; i < count && i < 10000; i++)
+    {
+        cout << output[i] << endl;
+    }
+    return 0;    
+}
+/*Given a string S, find and return all the possible permutations of the input string.
+Note 1 : The order of permutations is not important.
+Note 2 : If original string contains duplicate characters, permutations will also be duplicates.
+Input Format :
+String S
+Output Format :
+All permutations (in different lines)
+Sample Input :
+abc
+Sample Output :
+abc
+acb
+bac
+bca
+cab
+cba
+*/
